bf3bb3d2bca6dd512f7721ddc7f28b00bfec73ae
Suman Baral <ssumanbaral55@gmail.com>
2025-08-09 06:01:42 +0530

    City Database Schema added

---
+++ SRC/migrations/20250808185708-create-city.js
@@ -0,0 +1,28 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Cities", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  };
+  export async function down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Cities")
+  };
---
+++ SRC/models/city.js
@@ -0,0 +1,27 @@
+'use strict';
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  City.init({
+    name: {
+    type:DataTypes.STRING,
+    allowNull:false,
+    unique:true,
+    
+
+    }
+  }, {
+    sequelize,
+    modelName: 'City',
+  });
+  return City;
+};
