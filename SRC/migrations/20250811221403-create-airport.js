8003d569497861e9f5fd7becec10d6ffdf9962ea
Suman Baral <ssumanbaral55@gmail.com>
2025-08-12 04:00:03 +0530

    Added Basic Airport model and Migration Without Foreign Key constraint

---
+++ SRC/migrations/20250811221403-create-airport.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
+      },
+      address: {
+        type: Sequelize.STRING,
+        unique:true
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  }
+  export async function down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
